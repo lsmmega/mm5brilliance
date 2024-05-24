221708e316d764ae542b7d556a1086b69dee5633
lsmmega <pogmegalsm@outlook.com>
2024-05-24 17:32:59 +0800

    Create main.asm

---
+++ main.asm
@@ -0,0 +1,579 @@
+incsrc .\scripts\ines.asm
+incsrc header.asm
+prg_bank = 0
+base $8000
+pad $a000
+prg_bank = 1
+base $8000
+pad $a000
+prg_bank = 2
+base $8000
+pad $a000
+prg_bank = 3
+base $8000
+pad $a000
+prg_bank = 4
+base $8000
+pad $a000
+prg_bank = 5
+base $8000
+pad $a000
+prg_bank = 6
+base $8000
+pad $a000
+prg_bank = 7
+base $8000
+pad $a000
+prg_bank = 8
+base $8000
+pad $a000
+prg_bank = 9
+base $8000
+pad $a000
+prg_bank = 10
+base $8000
+pad $a000
+prg_bank = 11
+base $8000
+pad $a000
+prg_bank = 12
+base $8000
+pad $a000
+prg_bank = 13
+base $8000
+pad $a000
+prg_bank = 14
+base $8000
+pad $a000
+prg_bank = 15
+base $8000
+pad $a000
+prg_bank = 16
+base $8000
+pad $a000
+prg_bank = 17
+base $8000
+pad $a000
+prg_bank = 18
+base $8000
+pad $a000
+prg_bank = 19
+base $8000
+pad $a000
+prg_bank = 20
+base $8000
+pad $a000
+prg_bank = 21
+base $8000
+pad $a000
+prg_bank = 22
+base $8000
+pad $a000
+prg_bank = 23
+base $8000
+pad $a000
+prg_bank = 24
+base $8000
+pad $a000
+prg_bank = 25
+base $8000
+pad $a000
+prg_bank = 26
+base $8000
+pad $a000
+prg_bank = 27
+base $8000
+pad $a000
+prg_bank = 28
+base $8000
+pad $a000
+prg_bank = 29
+base $8000
+pad $a000
+prg_bank = 30
+base $8000
+pad $a000
+prg_bank = 31
+base $8000
+pad $a000
+prg_bank = 32
+base $8000
+pad $a000
+prg_bank = 33
+base $8000
+pad $a000
+prg_bank = 34
+base $8000
+pad $a000
+prg_bank = 35
+base $8000
+pad $a000
+prg_bank = 36
+base $8000
+pad $a000
+prg_bank = 37
+base $8000
+pad $a000
+prg_bank = 38
+base $8000
+pad $a000
+prg_bank = 39
+base $8000
+pad $a000
+prg_bank = 40
+base $8000
+pad $a000
+prg_bank = 41
+base $8000
+pad $a000
+prg_bank = 42
+base $8000
+pad $a000
+prg_bank = 43
+base $8000
+pad $a000
+prg_bank = 44
+base $8000
+pad $a000
+prg_bank = 45
+base $8000
+pad $a000
+prg_bank = 46
+base $8000
+pad $a000
+prg_bank = 47
+base $8000
+pad $a000
+prg_bank = 48
+base $8000
+pad $a000
+prg_bank = 49
+base $8000
+pad $a000
+prg_bank = 50
+base $8000
+pad $a000
+prg_bank = 51
+base $8000
+pad $a000
+prg_bank = 52
+base $8000
+pad $a000
+prg_bank = 53
+base $8000
+pad $a000
+prg_bank = 54
+base $8000
+pad $a000
+prg_bank = 55
+base $8000
+pad $a000
+prg_bank = 56
+base $8000
+pad $a000
+prg_bank = 57
+base $8000
+pad $a000
+prg_bank = 58
+base $8000
+pad $a000
+prg_bank = 59
+base $8000
+pad $a000
+prg_bank = 60
+base $8000
+pad $a000
+prg_bank = 61
+base $8000
+pad $a000
+prg_bank = 62
+base $8000
+pad $a000
+prg_bank = 63
+base $8000
+pad $a000
+prg_bank = 64
+base $8000
+pad $a000
+prg_bank = 65
+base $8000
+pad $a000
+prg_bank = 66
+base $8000
+pad $a000
+prg_bank = 67
+base $8000
+pad $a000
+prg_bank = 68
+base $8000
+pad $a000
+prg_bank = 69
+base $8000
+pad $a000
+prg_bank = 70
+base $8000
+pad $a000
+prg_bank = 71
+base $8000
+pad $a000
+prg_bank = 72
+base $8000
+pad $a000
+prg_bank = 73
+base $8000
+pad $a000
+prg_bank = 74
+base $8000
+pad $a000
+prg_bank = 75
+base $8000
+pad $a000
+prg_bank = 76
+base $8000
+pad $a000
+prg_bank = 77
+base $8000
+pad $a000
+prg_bank = 78
+base $8000
+pad $a000
+prg_bank = 79
+base $8000
+pad $a000
+prg_bank = 80
+base $8000
+pad $a000
+prg_bank = 81
+base $8000
+pad $a000
+prg_bank = 82
+base $8000
+pad $a000
+prg_bank = 83
+base $8000
+pad $a000
+prg_bank = 84
+base $8000
+pad $a000
+prg_bank = 85
+base $8000
+pad $a000
+prg_bank = 86
+base $8000
+pad $a000
+prg_bank = 87
+base $8000
+pad $a000
+prg_bank = 88
+base $8000
+pad $a000
+prg_bank = 89
+base $8000
+pad $a000
+prg_bank = 90
+base $8000
+pad $a000
+prg_bank = 91
+base $8000
+pad $a000
+prg_bank = 92
+base $8000
+pad $a000
+prg_bank = 93
+base $8000
+pad $a000
+prg_bank = 94
+base $8000
+pad $a000
+prg_bank = 95
+base $8000
+pad $a000
+prg_bank = 96
+base $8000
+pad $a000
+prg_bank = 97
+base $8000
+pad $a000
+prg_bank = 98
+base $8000
+pad $a000
+prg_bank = 99
+base $8000
+pad $a000
+prg_bank = 100
+base $8000
+pad $a000
+prg_bank = 101
+base $8000
+pad $a000
+prg_bank = 102
+base $8000
+pad $a000
+prg_bank = 103
+base $8000
+pad $a000
+prg_bank = 104
+base $8000
+pad $a000
+prg_bank = 105
+base $8000
+pad $a000
+prg_bank = 106
+base $8000
+pad $a000
+prg_bank = 107
+base $8000
+pad $a000
+prg_bank = 108
+base $8000
+pad $a000
+prg_bank = 109
+base $8000
+pad $a000
+prg_bank = 110
+base $8000
+pad $a000
+prg_bank = 111
+base $8000
+pad $a000
+prg_bank = 112
+base $8000
+pad $a000
+prg_bank = 113
+base $8000
+pad $a000
+prg_bank = 114
+base $8000
+pad $a000
+prg_bank = 115
+base $8000
+pad $a000
+prg_bank = 116
+base $8000
+pad $a000
+prg_bank = 117
+base $8000
+pad $a000
+prg_bank = 118
+base $8000
+pad $a000
+prg_bank = 119
+base $8000
+pad $a000
+prg_bank = 120
+base $8000
+pad $a000
+prg_bank = 121
+base $8000
+pad $a000
+prg_bank = 122
+base $8000
+pad $a000
+prg_bank = 123
+base $8000
+pad $a000
+prg_bank = 124
+base $8000
+pad $a000
+prg_bank = 125
+base $8000
+pad $a000
+prg_bank = 126
+base $c000
+pad $e000
+prg_bank = 127
+base $e000
+pad $10000
+;incsrc vectors.asm
+chr_bank = 0
+base $8000
+pad $c000
+chr_bank = 1
+base $8000
+pad $c000
+chr_bank = 2
+base $8000
+pad $c000
+chr_bank = 3
+base $8000
+pad $c000
+chr_bank = 4
+base $8000
+pad $c000
+chr_bank = 5
+base $8000
+pad $c000
+chr_bank = 6
+base $8000
+pad $c000
+chr_bank = 7
+base $8000
+pad $c000
+chr_bank = 8
+base $8000
+pad $c000
+chr_bank = 9
+base $8000
+pad $c000
+chr_bank = 10
+base $8000
+pad $c000
+chr_bank = 11
+base $8000
+pad $c000
+chr_bank = 12
+base $8000
+pad $c000
+chr_bank = 13
+base $8000
+pad $c000
+chr_bank = 14
+base $8000
+pad $c000
+chr_bank = 15
+base $8000
+pad $c000
+chr_bank = 16
+base $8000
+pad $c000
+chr_bank = 17
+base $8000
+pad $c000
+chr_bank = 18
+base $8000
+pad $c000
+chr_bank = 19
+base $8000
+pad $c000
+chr_bank = 20
+base $8000
+pad $c000
+chr_bank = 21
+base $8000
+pad $c000
+chr_bank = 22
+base $8000
+pad $c000
+chr_bank = 23
+base $8000
+pad $c000
+chr_bank = 24
+base $8000
+pad $c000
+chr_bank = 25
+base $8000
+pad $c000
+chr_bank = 26
+base $8000
+pad $c000
+chr_bank = 27
+base $8000
+pad $c000
+chr_bank = 28
+base $8000
+pad $c000
+chr_bank = 29
+base $8000
+pad $c000
+chr_bank = 30
+base $8000
+pad $c000
+chr_bank = 31
+base $8000
+pad $c000
+chr_bank = 32
+base $8000
+pad $c000
+chr_bank = 33
+base $8000
+pad $c000
+chr_bank = 34
+base $8000
+pad $c000
+chr_bank = 35
+base $8000
+pad $c000
+chr_bank = 36
+base $8000
+pad $c000
+chr_bank = 37
+base $8000
+pad $c000
+chr_bank = 38
+base $8000
+pad $c000
+chr_bank = 39
+base $8000
+pad $c000
+chr_bank = 40
+base $8000
+pad $c000
+chr_bank = 41
+base $8000
+pad $c000
+chr_bank = 42
+base $8000
+pad $c000
+chr_bank = 43
+base $8000
+pad $c000
+chr_bank = 44
+base $8000
+pad $c000
+chr_bank = 45
+base $8000
+pad $c000
+chr_bank = 46
+base $8000
+pad $c000
+chr_bank = 47
+base $8000
+pad $c000
+chr_bank = 48
+base $8000
+pad $c000
+chr_bank = 49
+base $8000
+pad $c000
+chr_bank = 50
+base $8000
+pad $c000
+chr_bank = 51
+base $8000
+pad $c000
+chr_bank = 52
+base $8000
+pad $c000
+chr_bank = 53
+base $8000
+pad $c000
+chr_bank = 54
+base $8000
+pad $c000
+chr_bank = 55
+base $8000
+pad $c000
+chr_bank = 56
+base $8000
+pad $c000
+chr_bank = 57
+base $8000
+pad $c000
+chr_bank = 58
+base $8000
+pad $c000
+chr_bank = 59
+base $8000
+pad $c000
+chr_bank = 60
+base $8000
+pad $c000
+chr_bank = 61
+base $8000
+pad $c000
+chr_bank = 62
+base $8000
+pad $c000
+chr_bank = 63
+base $8000
+pad $c000
